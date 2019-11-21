f6becab01b146c01a65fe8e6fd2e5c267e9e0f02
Edd Tubbs <ed@silesiacoin.org>
2019-11-21 14:46:47 +0100

    Spike: redis-on-nodejs

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var bodyParser = require('body-parser');
-
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const bodyParser = require('body-parser');
+const redis = require('redis');
+const { promisify } = require('util');
 app.set('port', (process.env.PORT || 3000));
 
+app.set('redisHost', (process.env.REDIS_HOST || 'redis'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+const redisClient = redis.createClient(process.env.REDIS_HOST);
+const getAsync = promisify(redisClient.get).bind(redisClient);
 
 /**
  * Configuration from package.json
  */
-var pjson = require('./package.json');
+const pjson = require('./package.json');
 
-var pushService = (function() {
-	var connections = {};
+const pushService = (function () {
+	const connections = {};
 	return {
 		/**
 		 * Register user in connections. This method must be executed as first in whole registration process.
 		 * @param userId id of user.
 		 * @param connectionId id of connection.
 		 */
-		registerUser: function(userId, connectionId) {
+		registerUser: function (userId, connectionId) {
 			if (connections[userId] === undefined) {
 				connections[userId] = {};
 			}
@@ -37,7 +42,7 @@ var pushService = (function() {
 		 * @param socket socket.
 		 * @returns {boolean} if socket was registered or not, if false then you have to do everything again.
 		 */
-		registerSocket: function(userId, connectionId, socket) {
+		registerSocket: function (userId, connectionId, socket) {
 			if (connections[userId] != null && connections[userId][connectionId] == null) {
 				socket.userId = userId;
 				socket.connectionId = connectionId;
@@ -53,9 +58,9 @@ var pushService = (function() {
 		 * Remove connection.
 		 * @param socket socket to remove.
 		 */
-		removeConnection: function(socket) {
-			var userId = socket.userId;
-			var connectionId = socket.connectionId;
+		removeConnection: function (socket) {
+			const userId = socket.userId;
+			const connectionId = socket.connectionId;
 			if (userId && connectionId && connections[userId] && connections[userId][connectionId]) {
 				console.log('Removed socket for user ' + userId + ' and connection: ' + connectionId.substring(0, 4) + '***');
 				delete connections[socket.connectionId];
@@ -66,12 +71,12 @@ var pushService = (function() {
 		 * @param userId id of user.
 		 * @param message message.
 		 */
-		pushMessage: function(userId, message) {
-			var userConnections = connections[userId];
+		pushMessage: function (userId, message) {
+			const userConnections = connections[userId];
 			if (userConnections) {
-				for (var connectionId in  userConnections) {
+				for (const connectionId in userConnections) {
 					if (userConnections.hasOwnProperty(connectionId)) {
-						var socket = userConnections[connectionId];
+						const socket = userConnections[connectionId];
 						if (socket != null) {
 							socket.emit('message', message);
 						}
@@ -105,11 +110,11 @@ io.on('connection', function(socket) {
  * Api to register user.
  */
 app.put('/api/:userId/register', function(req, res) {
-	if (req.header('X-AUTH-TOKEN') != process.env['AUTH_TOKEN']) {
+	if (req.header('X-AUTH-TOKEN') !== process.env['AUTH_TOKEN']) {
 		res.status(401).send();
 	} else {
-		var userId = req.params['userId'];
-		var connectionId = req.query['connectionId'];
+		const { userId } = req.params;
+		const { connectionId } = req.query;
 		if (userId && connectionId) {
 			pushService.registerUser(userId, connectionId);
 			res.send();
@@ -123,10 +128,10 @@ app.put('/api/:userId/register', function(req, res) {
  * Api to send message to user.
  */
 app.post('/api/:userId/push', function(req, res) {
-	if (req.header('X-AUTH-TOKEN') != process.env['AUTH_TOKEN']) {
+	if (req.header('X-AUTH-TOKEN') !== process.env['AUTH_TOKEN']) {
 		res.status(401).send();
 	} else {
-		var userId = req.params['userId'];
+		const { userId } = req.params;
 		if (userId && req.body.message) {
 			pushService.pushMessage(userId, req.body.message);
 			res.send();
@@ -149,13 +154,53 @@ app.get('/api/status/ping', function(req, res) {
  */
 app.get('/api/status/info', function(req, res) {
 	res.setHeader('Content-Type', 'application/json');
-	var info = {
+	const info = {
 		'name': pjson.name,
 		'version': pjson.version
 	};
 	res.send(info)
 });
 
+/**
+ * Redis check endpoint
+ */
+app.get('/api/redis/:redisKey', async function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	const { params, body } = req;
+	const { redisKey } = params;
+
+	return res.json(await handleRedis(redisKey, body));
+});
+
+/**
+ * Redis write endpoint
+ */
+app.put('/api/redis/:redisKey', async function (req, res) {
+	res.setHeader('Content-Type', 'application/json');
+
+	const { params, body } = req;
+	const { redisKey } = params;
+
+	return res.json(await handleRedis(redisKey, body, true));
+});
+
+async function handleRedis(redisKey, redisValue, override = false) {
+	if (override) {
+		redisClient.setex(redisKey, 3600, JSON.stringify(redisValue));
+
+		return { source: 'api', data: redisValue };
+	}
+
+	const fetchedRedisValue = await getAsync(redisKey);
+
+	return { source: 'cache', data: JSON.parse(fetchedRedisValue)};
+}
+
 http.listen(app.get('port'), function() {
 	console.log('Node app is running on port', app.get('port'));
+});
+
+redisClient.on('error', (err) => {
+	console.log('\n[REDIS-ERROR]: ', err.message);
 });
